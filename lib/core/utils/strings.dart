d1fe6b8ae351f04913694f4203efb2553a025242
Abo Zeid <hamoashraf1960@gmail.com>
2022-12-24 14:19:25 +0200

    TV

--- lib/core/utils/strings.dart
+++ lib/core/utils/strings.dart
@@ -3,6 +3,7 @@ class AppStrings {
   static const String noInternet = 'No Internet Connection';
   static const String refresh = 'Refresh';
   static const String signIn = 'Sign in';
+  static const String signOut = 'Sign out';
   static const String emailAddress = 'Email Address';
   static const String password = 'Password';
   static const String login = 'Login';
--- lib/features/fury/presentation/screens/settings/settings_screen.dart
+++ lib/features/fury/presentation/screens/settings/settings_screen.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:movies_application/core/utils/strings.dart';
+
+import '../../../../../core/widgets/button.dart';
 
 class SettingsScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
+        DefaultButton(
+          fun: () {
 
+          },
+          text: AppStrings.signOut,
+          height: 100,
+          fontSize: 20,
+          borderRadius: 0,
+        )
       ],
     );
   }
